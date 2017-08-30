3e75ea787408688b3446a93047e07bc731e30f05
Sadeq Naqashzadeh <SadeqN@GMail.com>
2017-08-30 22:28:24 +0430

    do not stop working if there is not idn tool

--- domain-check-2.sh
+++ domain-check-2.sh
@@ -171,7 +171,7 @@ DATE=`which date`
 CUT=`which cut`
 TR=`which tr`
 MAIL=`which mail`
-IDN=`which idn`
+IDN=`which idnxxx`; [ -z "$IDN" ] && IDN="cat"
 GREP=`which egrep`
 
 # Place to stash temporary files
