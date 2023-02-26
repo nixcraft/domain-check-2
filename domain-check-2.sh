31db7e06327b1c8fcd45992d8fc992097ff66af0
vlad11 <admin@support.od.ua>
2023-02-26 21:22:43 +0200

    Added support for .sn TLDs.
    Signed-off-by: vlad11 <admin@support.od.ua>

--- domain-check-2.sh
+++ domain-check-2.sh
@@ -1075,6 +1075,15 @@ check_domain_status()
        tday=$(echo ${tdomdate} | ${CUT} -d'/' -f2)
        DOMAINDATE=`echo ${tday}-${tmonth}-${tyear}`
 
+    elif [ "${TLDTYPE}" == "sn" ];
+    then
+       tdomdate=`${AWK} -F: '/expiration:/ { sub(/^[ \t]+/,"",$2); sub(/T[0-9]+/,"",$2); print $2 }' ${WHOIS_TMP} | ${TR} -d " \r"`
+       tyear=$(echo ${tdomdate} | ${CUT} -d'-' -f1)
+       tmon=$(echo ${tdomdate} | ${CUT} -d'.' -f2)
+       tmonth=$(getmonth_number ${tmon})
+       tday=$(echo ${tdomdate} | ${CUT} -d'-' -f3)
+       DOMAINDATE=`echo ${tday}-${tmonth}-${tyear}`
+
     # may work with others	 ??? ;)
     else
         DOMAINDATE=`${AWK} '/Expiration:|[Ee]xpires:|Registry Expiry Date:|Registrar Registration Expiration Date:/ \
